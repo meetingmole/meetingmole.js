43923f5bbcdb56e817ba885a36cb5f4818aa41a8
Joni Stolt <joni.stolt@googlemail.com>
2016-02-15 18:16:38 +0100

    Added hook to automatic package from VS build.

--- MeetingMole API JavaScript Client/meetingmole.js/gruntfile.js
+++ MeetingMole API JavaScript Client/meetingmole.js/gruntfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='cssmin:all, uglify:all' ProjectOpened='bower:install' />
+﻿/// <binding AfterBuild='cssmin:all, uglify:all, copy:package' ProjectOpened='bower:install' />
 /*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
